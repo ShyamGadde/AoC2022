6320bb6a08d99fc7afe351b83dc1aca190d64711
Shyamsundar Gadde <shyamgadde1602@gmail.com>
2022-12-04 11:57:13 +0530

    Add solution for Day 4 Part 2

--- day04/solution.py
+++ day04/solution.py
@@ -5,10 +5,10 @@ with open("input.txt") as file:
 overlaps = 0
 intersections = 0
 for section in sections:
-    assignment1_str = list(map(int, section[0].split("-")))
-    assignment2_str = list(map(int, section[1].split("-")))
-    assignment1 = set(range(assignment1_str[0], assignment1_str[1] + 1))
-    assignment2 = set(range(assignment2_str[0], assignment2_str[1] + 1))
+    assignment1_range = list(map(int, section[0].split("-")))
+    assignment2_range = list(map(int, section[1].split("-")))
+    assignment1 = set(range(assignment1_range[0], assignment1_range[1] + 1))
+    assignment2 = set(range(assignment2_range[0], assignment2_range[1] + 1))
     if assignment1.issubset(assignment2) or assignment2.issubset(assignment1):
         overlaps += 1
 
